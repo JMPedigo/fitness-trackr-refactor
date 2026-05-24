1094d95e0ff23fa741ec37d60fb359a5f88ddc57
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-24 13:18:57 -0500

    Creates RoutineListItem

--- src/routines/RoutinesList.jsx
+++ src/routines/RoutinesList.jsx
@@ -1,3 +1,20 @@
-export default function RoutinesList() {
-  return <div>RoutinesList</div>;
+/** I need to map routines into an unordered list, with a list item that returns a routine */
+export default function RoutinesList({ routines }) {
+  return (
+    <ul>
+      {routines.map((routine) => (
+        <RoutineListItem key={routine.id} routine={routine} />
+      ))}
+    </ul>
+  );
+}
+
+/** I need a routine list item that creates a link to the specified routine */
+
+function RoutineListItem(routine) {
+  return (
+    <li>
+      <Link to={`/routines/${routine.id}`}>{routine.name}</Link>
+    </li>
+  );
 }
