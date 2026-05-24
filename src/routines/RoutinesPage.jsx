e75d02be6a953ba5aa87327ff8444114b4542649
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-24 12:57:23 -0500

    Rleshes out RoutinesPage to get and set routines

--- src/routines/RoutinesPage.jsx
+++ src/routines/RoutinesPage.jsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+import { getRoutines } from "../api/routines";
+
 export default function RoutinesPage() {
-  return <div>RoutinesPage</div>;
+  /**I need to create routines and setRoutines*/
+  const [routines, setRoutines] = useState([]);
+
+  const syncRoutines = async () => {
+    const data = await getRoutines();
+    setRoutines(data);
+  };
+
+  useEffect(() => {
+    syncRoutines();
+  }, []);
+
+  return (
+    <>
+      <hi>Routines</hi>
+    </>
+  );
 }
