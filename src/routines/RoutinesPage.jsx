81d8a6c83207f71280dbd8f43b56979de53fd2c2
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-24 13:02:23 -0500

    Adds RoutinesList to return element

--- src/routines/RoutinesPage.jsx
+++ src/routines/RoutinesPage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { getRoutines } from "../api/routines";
+import ActivityList from "../activities/ActivityList";
+import RoutinesList from "./RoutinesList";
 
 export default function RoutinesPage() {
   /**I need to create routines and setRoutines*/
@@ -17,6 +19,7 @@ export default function RoutinesPage() {
   return (
     <>
       <hi>Routines</hi>
+      <RoutinesList routines={routines} syncRoutines={syncRoutines} />
     </>
   );
 }
