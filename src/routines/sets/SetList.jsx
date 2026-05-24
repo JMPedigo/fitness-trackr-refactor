ac0bc06da6f90b3ff0dd7ea5c5aafb06c1f69f18
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-24 18:35:36 -0500

    creates SetList component

--- src/routines/sets/SetList.jsx
+++ src/routines/sets/SetList.jsx
@@ -1,3 +1,13 @@
-export default function SetList() {
-  return <div>SetList</div>;
+/** I need a list of sets,
+ * with an activity from the activity list,
+ * and a delete set button.
+ */
+export default function SetList({ sets, syncRoutine }) {
+  return (
+    <ul>
+      {sets.map((set) => (
+        <SetListItem key={set.id} set={set} syncRoutine={syncRoutine} />
+      ))}
+    </ul>
+  );
 }
