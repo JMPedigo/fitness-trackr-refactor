f15f8aaccf6ecb406732cd023b4698e942ef8bde
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-24 13:38:29 -0500

    Constructs RoutineDetails function

--- src/routines/RoutineDetails.jsx
+++ src/routines/RoutineDetails.jsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getRoutines } from "../api/routines";
+
+/**I need a function to call RoutineDetails and grab id using useParams */
 export default function RoutineDetails() {
-  return <div>RoutineDetails</div>;
+  const { id } = useParams();
+  const [routine, setRoutine] = useState(null);
+
+  /**I need to call getRoutine and setRoutine to obtain and change its data */
+  useEffect(() => {
+    const syncRoutine = async () => {
+      const data = await getRoutines(id);
+      setRoutine(data);
+    };
+    syncRoutine();
+  }, [id]);
+
+  /**I need to return an element that provides routine name, creator and description */
+  return (
+    <article>
+      <h1>{routine?.name}</h1>
+      <p>Created by: {routine?.creatorName}</p>
+      <p>{routine?.description}</p>
+    </article>
+  );
 }
