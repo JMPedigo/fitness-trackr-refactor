b92fa7e93bef90d723a7054aa9ce68f71e62ec1b
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-24 17:32:36 -0500

    Constructs the RoutineForm component function

--- src/routines/RoutineForm.jsx
+++ src/routines/RoutineForm.jsx
@@ -1,8 +1,26 @@
-/** I need a component that allows a user to create a new routine*/ 
-export default function RoutineForm() {
-  return (
-    <div>RoutineForm</div>
-  )
-}
+import { useAuth } from "../auth/AuthContext";
+import { createRoutine } from "../api/routines";
+import { useState } from "react";
+/** I need a form that allows a user to create a new routine with a name and goal.
+ * Requires a valid token.*/
+export default function RoutineForm({ routine }) {
+  const { token } = useAuth();
 
-export default 
+  const [error, setError] = useState(null);
+
+  const tryCreateRoutine = async (formData) => {
+    setError(null);
+
+    const name = formData.get("name");
+    const goal = formData.get("goal");
+
+    try {
+      await createRoutine(token, { name, goal });
+      syncRoutines();
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return <></>;
+}
