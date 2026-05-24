8814423c11c8aa29e6ef85491835c9a57951c1f4
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-24 17:56:34 -0500

    Creates tryDeleteRoutine

--- src/routines/RoutineDetails.jsx
+++ src/routines/RoutineDetails.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { getRoutine } from "../api/routines";
+import { useNavigate, useParams } from "react-router";
+import { deleteRoutine, getRoutine } from "../api/routines";
+import { useAuth } from "../auth/AuthContext";
 
 /**I need a function to call RoutineDetails and grab id using useParams */
 export default function RoutineDetails() {
+  const { token } = useAuth();
   const { id } = useParams();
+  const navigate = useNavigate();
   const [routine, setRoutine] = useState(null);
+  const [error, setError] = useState(null);
 
   /**I need to call getRoutine and setRoutine to obtain and change its data */
   useEffect(() => {
@@ -15,6 +19,16 @@ export default function RoutineDetails() {
     };
     syncRoutine();
   }, [id]);
+
+  /**I need to request the API to delete a routine */
+  const tryDeleteRoutine = async () => {
+    try {
+      await deleteRoutine(token, routine.id);
+      navigate("/routines");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
 
   /**I need to return an element that provides routine name, creator and description */
   return (
