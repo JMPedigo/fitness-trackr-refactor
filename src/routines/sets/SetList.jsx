4608ea0d7a519aa727b57ac4c89205d95a8ad84b
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-24 19:05:40 -0500

    Creates SetList and SetListItem, and the delete button

--- src/routines/sets/SetList.jsx
+++ src/routines/sets/SetList.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
+import { useAuth } from "../../auth/AuthContext";
+
 /** I need a list of sets,
  * with an activity from the activity list,
  * and a delete set button.
+ * Need a conditional ternary to display a list of sets, or a message if there are none.
  */
 export default function SetList({ sets, syncRoutine }) {
   return (
-    <ul>
-      {sets.map((set) => (
-        <SetListItem key={set.id} set={set} syncRoutine={syncRoutine} />
-      ))}
-    </ul>
+    <>
+      <h3>Sets</h3>
+      {sets.length > 0 ? (
+        <ul>
+          {sets.map((set) => (
+            <SetListItem key={set.id} set={set} syncRoutine={syncRoutine} />
+          ))}
+        </ul>
+      ) : (
+        <p>This routine doesn't have any sets. Add one?</p>
+      )}
+    </>
+  );
+}
+/** I need a list item that contains an activity name * count
+ * and a delete set button
+ * that requires a valid token.
+ */
+function SetListItem({ set, syncRoutine }) {
+  const { token } = useAuth();
+
+  const [error, setError] = useState(null);
+
+  const tryDeleteSet = async () => {
+    setError(null);
+  };
+  return (
+    <li>
+      <p>
+        {set.name} 🅧 {set.count}
+      </p>
+      {token && <button onClick={tryDeleteSet}>Delete set</button>}
+      {error && <p role="alert">{error}</p>}
+    </li>
   );
 }
